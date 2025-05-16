1a7b4548e24bd0a3b00dca08936580b9391c3de3
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2025-05-16 12:57:14 +0530

    learning GO day-1

---
+++ practice02/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1)
+	//int
+	fmt.Println(1 + 1)
+	//string
+	fmt.Println("hello world")
+	//boolean
+	fmt.Println(true)
+	fmt.Println(false)
+	//float
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+}
---
+++ practice03/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name1 string = "golang"
+	//infer
+	// var name2 = "golang"
+	//bool
+	// var isAdult = true
+	//int
+	// var age = 19
+
+	//sorthand
+	name := "golang"
+
+	var name3 string
+	name3 = "golang(name3)"
+
+	// fmt.Println(name1)
+	// fmt.Println(name2)
+	// fmt.Println(isAdult)
+	// fmt.Println(age)
+	fmt.Println(name)
+	fmt.Println(name3)
+}
