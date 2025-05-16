f7b07bccafc2e6784a2a75c9a95c9d8ad063c49e
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2025-05-16 13:09:30 +0530

    learning GO day-1

---
+++ for/for.go
@@ -0,0 +1,38 @@
+package main
+
+// for -> only construct in go for looping
+func main() {
+
+	//while loop
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	//infinite loop
+	// for {
+	// 	fmt.Println(1)
+	// }
+
+	// classic for loop
+	// for i := 0; i <= 3; i++ {
+
+	// break
+	// if i == 2 {
+	// 	break
+	// }
+
+	// continue
+	// if i == 1 {
+	// 	continue
+	// }
+	// fmt.Println(i)
+	// }
+
+	// Range
+	// for i := range 6 {
+	// 	fmt.Println(i)
+	// }
+
+}
